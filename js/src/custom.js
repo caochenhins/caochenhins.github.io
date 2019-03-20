f90212d984779a10af2db32d4b1a94fd10ff375a
caoc <ipod_nano@qq.com>
2019-03-21 00:23:25 +0800

    Site updated: 2019-03-21 00:23:24

--- js/src/custom.js
+++ js/src/custom.js
@@ -80,8 +80,8 @@ jQuery(document).ready(function($) {
 /* 轮播背景图片 */
 $(function () {
 	$.backstretch([
-		  "/images/background/saber1.jpg",
-		  "/images/background/saber2.jpg",
+		  //"/images/background/saber1.jpg",
+		  //"/images/background/saber2.jpg",
 		  "/images/background/wlop.jpg"
 	], { duration: 60000, fade: 1500 });
 });
